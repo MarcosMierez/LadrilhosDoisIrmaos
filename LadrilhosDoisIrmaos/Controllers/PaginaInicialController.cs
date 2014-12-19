64301ba7d897148ff3722feaa101c85f93c78549
Marcos Vinicius <ka-ladze@hotmail.com>
2014-12-19 20:14:10 -0200

    criando o html da pagina inicial

---
+++ LadrilhosDoisIrmaos/Controllers/PaginaInicialController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LadrilhosDoisIrmaos.Controllers
+{
+    public class PaginaInicialController : Controller
+    {
+        // GET: PaginaInicial
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
